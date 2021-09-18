f3a3896421a716ab3a35803917219e5238b380b5
Shad Kaske <shadkaske@gmail.com>
2021-09-18 08:04:25 -0500

    Updated sxhkd to use with qtile and others

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -1,77 +1,39 @@
+--[[
+lvim is the global options object
+
+Linters should be
+filled in as strings with either
+a global executable or a path to
+an executable
+]]
 -- THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
 
 -- general
 lvim.log.level = "warn"
-lvim.format_on_save = false
-lvim.lint_on_save = true
--- lvim.colorscheme = "onedarker"
-lvim.colorscheme = "monokai_pro"
-
--- My preferred settings
-local gblopt = vim.o
--- local buf = vim.bo
--- local win = vim.wo
-gblopt.relativenumber = true
-gblopt.number = true
-gblopt.scrolloff = 3
-gblopt.colorcolumn = "80,120"
-
--- Import vim script keys that I haven't converted to lua
-vim.cmd('source ~/.config/lvim/keys.vim')
+lvim.format_on_save = true
+lvim.colorscheme = "onedarker"
 
 -- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
 -- add your own keymapping
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
-
--- TmuxNavigator
-lvim.keys.normal_mode["<C-h>"] = ":lua require('Navigator').left()<cr>"
-lvim.keys.normal_mode["<C-k>"] = ":lua require('Navigator').up()<cr>"
-lvim.keys.normal_mode["<C-l>"] = ":lua require('Navigator').right()<cr>"
-lvim.keys.normal_mode["<C-j>"] = ":lua require('Navigator').down()<cr>"
--- lvim.keys.normal_mode["<C-p>"] = ":lua require('Navigator').previous()<cr>"
-
--- Telescope
-lvim.keys.normal_mode["<C-p>"] = "<cmd>Telescope git_files<CR>"
-
--- Float Term
-local map = vim.api.nvim_set_keymap
-local keyopts = { noremap = true, silent = true }
--- local keyexpr = { noremap = true, silent = true, expr = true }
-
-map('n', '<A-i>', '<CMD>lua require("FTerm").toggle()<cr>', keyopts)
-map('t', '<A-i>', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>', keyopts)
--- Yank End of Line like D or C
-map('n', 'Y', 'y$', keyopts)
--- Recenter find next/prev jumps
-map('n', 'n', 'nzzzv', keyopts)
-map('n', 'N', 'Nzzzv', keyopts)
--- Undo break points
-map('i', ',', ',<c-g>u', keyopts)
-map('i', '.', '.<c-g>u', keyopts)
-map('i', '!', '!<c-g>u', keyopts)
-map('i', '?', '?<c-g>u', keyopts)
-map('i', ')', '?<c-g>u', keyopts)
-map('i', '(', '?<c-g>u', keyopts)
--- Add larger j / k moves to jump list
-
 -- unmap a default keymapping
 -- lvim.keys.normal_mode["<C-Up>"] = ""
 -- edit a default keymapping
 -- lvim.keys.normal_mode["<C-q>"] = ":q<cr>"
 
 -- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
-lvim.builtin.telescope.on_config_done = function()
-  local actions = require "telescope.actions"
+-- lvim.builtin.telescope.on_config_done = function()
+--   local actions = require "telescope.actions"
 --   -- for input mode
-  lvim.builtin.telescope.defaults.mappings.i["<C-n>"] = actions.move_selection_next
-  lvim.builtin.telescope.defaults.mappings.i["<C-p>"] = actions.move_selection_previous
+--   lvim.builtin.telescope.defaults.mappings.i["<C-j>"] = actions.move_selection_next
+--   lvim.builtin.telescope.defaults.mappings.i["<C-k>"] = actions.move_selection_previous
 --   lvim.builtin.telescope.defaults.mappings.i["<C-n>"] = actions.cycle_history_next
 --   lvim.builtin.telescope.defaults.mappings.i["<C-p>"] = actions.cycle_history_prev
 --   -- for normal mode
-  lvim.builtin.telescope.defaults.mappings.n["<C-n>"] = actions.move_selection_next
-  lvim.builtin.telescope.defaults.mappings.n["<C-p>"] = actions.move_selection_previous
-end
+--   lvim.builtin.telescope.defaults.mappings.n["<C-j>"] = actions.move_selection_next
+--   lvim.builtin.telescope.defaults.mappings.n["<C-k>"] = actions.move_selection_previous
+-- end
 
 -- Use which-key to add extra bindings with the leader-key prefix
 -- lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
@@ -79,90 +41,21 @@ end
 --   name = "+Trouble",
 --   r = { "<cmd>Trouble lsp_references<cr>", "References" },
 --   f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
---   d = { "<cmd>Trouble lsp_document_diagnostics<cr>", "Diagnosticss" },
+--   d = { "<cmd>Trouble lsp_document_diagnostics<cr>", "Diagnostics" },
 --   q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
 --   l = { "<cmd>Trouble loclist<cr>", "LocationList" },
---   w = { "<cmd>Trouble lsp_workspace_diagnostics<cr>", "Diagnosticss" },
+--   w = { "<cmd>Trouble lsp_workspace_diagnostics<cr>", "Diagnostics" },
 -- }
-lvim.builtin.which_key.mappings.g.n = {"<cmd>Neogit<cr>", "Neogit"}
-lvim.builtin.which_key.mappings["."] = { "<cmd>nohlsearch<cr>", "No Highlight"}
 
 -- TODO: User Config for predefined plugins
-lvim.plugins = {
-  {
-    'numToStr/Navigator.nvim',
-    config = function()
-        require('Navigator').setup()
-    end
-  },
-  {
-    "blackCauldron7/surround.nvim",
-    config = function()
-      require"surround".setup {mappings_style = "surround"}
-    end
-  },
-  {
-    'TimUntersberger/neogit', requires = 'nvim-lua/plenary.nvim'
-  },
-  {
-    'jghauser/mkdir.nvim',
-    config = function()
-      require('mkdir')
-    end
-  },
-  {
-    "phaazon/hop.nvim",
-    event = "BufRead",
-    config = function()
-      require("hop").setup()
-      vim.api.nvim_set_keymap("n", "s", ":HopChar2<cr>", { silent = true })
-      vim.api.nvim_set_keymap("n", "S", ":HopWord<cr>", { silent = true })
-    end,
-  },
-  {
-    "glepnir/indent-guides.nvim"
-  },
-  {
-    "norcalli/nvim-colorizer.lua",
-      config = function()
-        require("colorizer").setup({ "*" }, {
-            RGB = true, -- #RGB hex codes
-            RRGGBB = true, -- #RRGGBB hex codes
-            RRGGBBAA = true, -- #RRGGBBAA hex codes
-            rgb_fn = true, -- CSS rgb() and rgba() functions
-            hsl_fn = true, -- CSS hsl() and hsla() functions
-            css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-            css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
-            })
-    end,
-  },
-  {
-    "shadkaske/vim-blade"
-  },
-  {
-    "sainnhe/edge"
-  },
-  {
-    "tomasiser/vim-code-dark"
-  },
-  {
-    "phanviet/vim-monokai-pro"
-  }
-}
-
-require('Navigator').setup({
-    auto_save = 'current',
-    disable_on_zoom = true
-})
-
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
 lvim.builtin.dashboard.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.side = "left"
-lvim.builtin.nvimtree.show_icons.git = 1
+lvim.builtin.nvimtree.show_icons.git = 0
 
 -- if you don't want all the parsers change this to a table of the ones you want
-lvim.builtin.treesitter.ensure_installed = {}
+lvim.builtin.treesitter.ensure_installed = "maintained"
 lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enabled = true
 
@@ -196,29 +89,25 @@ lvim.builtin.treesitter.highlight.enabled = true
 -- lvim.lang.python.formatters = {
 --   {
 --     exe = "black",
---     args = {}
 --   }
 -- }
 -- set an additional linter
 -- lvim.lang.python.linters = {
 --   {
 --     exe = "flake8",
---     args = {}
 --   }
 -- }
 
 -- Additional Plugins
 -- lvim.plugins = {
---     {"folke/tokyonight.nvim"}, {
---         "ray-x/lsp_signature.nvim",
---         config = function() require"lsp_signature".on_attach() end,
---         event = "InsertEnter"
---     }
+--     {"folke/tokyonight.nvim"},
+--     {
+--       "folke/trouble.nvim",
+--       cmd = "TroubleToggle",
+--     },
 -- }
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
-lvim.autocommands.custom_groups = {
-  { "BufWinEnter", "*.lua", "setlocal ts=2 sw=2 expandtab" },
-  { "BufWinEnter", "*.php", "setlocal ts=4 sw=4 expandtab" },
-  -- { "Filetype" html,css,blade EmmetInstall }
-}
+-- lvim.autocommands.custom_groups = {
+--   { "BufWinEnter", "*.lua", "setlocal ts=8 sw=8" },
+-- }
