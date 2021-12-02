db34a1d57fcf65168b620be1e5c517c0d7423046
Shad Kaske <shadkaske@gmail.com>
2021-12-02 10:48:11 -0600

    Added Super+q to close in Awesome

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -492,6 +492,8 @@ clientkeys = my_table.join(
             c:raise()
         end,
         {description = "toggle fullscreen", group = "client"}),
+    awful.key({ modkey            }, "q",      function (c) c:kill()                         end,
+              {description = "close", group = "client"}),
     awful.key({ modkey, "Shift"   }, "c",      function (c) c:kill()                         end,
               {description = "close", group = "client"}),
     awful.key({ modkey, "Control" }, "space",  awful.client.floating.toggle                     ,
