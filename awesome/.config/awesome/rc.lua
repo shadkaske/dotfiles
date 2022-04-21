a96a33e7419cfc0ae93e1206a785d7428e91eda1
Shad Kaske <shadkaske@gmail.com>
2022-04-21 10:24:49 -0500

    Added back default close window shortcut in awesome

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -674,6 +674,8 @@ clientkeys = mytable.join(
         {description = "toggle fullscreen", group = "client"}),
     awful.key({ modkey,           }, "q",      function (c) c:kill()                         end,
               {description = "close", group = "client"}),
+    awful.key({ modkey, "Shift"   }, "c",      function (c) c:kill()                         end,
+              {description = "close", group = "client"}),
     awful.key({ modkey, "Control" }, "space",  awful.client.floating.toggle                     ,
               {description = "toggle floating", group = "client"}),
     awful.key({ modkey, "Control" }, "Return", function (c) c:swap(awful.client.getmaster()) end,
