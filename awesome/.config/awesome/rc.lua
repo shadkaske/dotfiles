cc06ceea40fee8bc635c862ea42531601c0fc840
Shad Kaske <shadkaske@gmail.com>
2022-12-22 16:01:47 -0600

    removed unused lines in rc.lua

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -11,7 +11,6 @@
 -- found (e.g. lgi). If LuaRocks is not installed, do nothing.
 pcall(require, "luarocks.loader")
 
-local awesome = require("awesome")
 local gears = require("gears")
 local awful = require("awful")
 require("awful.autofocus")
@@ -19,7 +18,6 @@ local wibox         = require("wibox")
 local beautiful     = require("beautiful")
 local naughty       = require("naughty")
 local lain          = require("lain")
---local menubar       = require("menubar")
 local freedesktop   = require("freedesktop")
 local hotkeys_popup = require("awful.hotkeys_popup")
 require("awful.hotkeys_popup.keys")
