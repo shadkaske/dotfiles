7d981f648eacd6b18158a88ea57926ab8b1c2b43
Shad Kaske <shadkaske@gmail.com>
2022-12-22 19:33:38 -0600

    Got rid of the defaults.lua moved loading to init

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -44,7 +44,21 @@ require("autocmd")
 -- Import Custom functions
 -- require("functions")
 
--- Example custom configuration for lua
+-- Loop through the files in lua/config to load plugin settings
+local configs_dir = vim.fn.stdpath("config") .. "/lua/config"
+
+for _, file in pairs(vim.fn.readdir(configs_dir)) do
+  if vim.fn.filereadable(file) then
+    require("config." .. file:match("(.+)%..+$"))
+  end
+end
+
+-- Load ColorScheme
+-- To set up additional color themes, install in plugins.lua and add a name
+--  lua config file in lua/colors and update the require statement below
+require("colors.onedark")
+
+-- sumneko_lua Config
 --
 -- Make runtime files discoverable to the server
 local runtime_path = vim.split(package.path, ";")
@@ -69,50 +83,6 @@ require("lspconfig").sumneko_lua.setup({
       -- Do not send telemetry data containing a randomized but unique identifier
       telemetry = { enable = false },
     },
-  },
-})
-
--- nvim-cmp setup
-local cmp = require("cmp")
-local luasnip = require("luasnip")
-
-cmp.setup({
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end,
-  },
-  mapping = cmp.mapping.preset.insert({
-    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    ["<C-Space>"] = cmp.mapping.complete(),
-    ["<CR>"] = cmp.mapping.confirm({
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
-    }),
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-  }),
-  sources = {
-    { name = "nvim_lsp" },
-    { name = "luasnip" },
-    { name = "path" },
   },
 })
 
---
+++ nvim/.config/nvim/lua/colors/onedark.lua
@@ -0,0 +1,11 @@
+local onedark_ok, onedark = pcall(require, "onedark")
+
+if not onedark_ok then
+  return
+end
+
+onedark.setup({
+  style = "darker",
+})
+
+onedark.load()
