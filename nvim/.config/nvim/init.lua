ea2ee720783235c4d801b6fa5d6edbe3f7764d77
Shad Kaske <shadkaske@gmail.com>
2022-12-22 19:42:14 -0600

    Function for require a directory of files
    Allows for lsp configs and plugin configs to currently be required by
    simply placing files in the directory.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -44,47 +44,25 @@ require("autocmd")
 -- Import Custom functions
 -- require("functions")
 
--- Loop through the files in lua/config to load plugin settings
-local configs_dir = vim.fn.stdpath("config") .. "/lua/config"
+local function require_all_files(required)
+  local directory = vim.fn.stdpath("config") .. "/lua/" .. required
 
-for _, file in pairs(vim.fn.readdir(configs_dir)) do
-  if vim.fn.filereadable(file) then
-    require("config." .. file:match("(.+)%..+$"))
+  for _, file in pairs(vim.fn.readdir(directory)) do
+    require(required .. "." .. file:match("(.+)%..+$"))
   end
+end
+
+-- Loop through the files in lua/config to load plugin settings
+local config_dirs = { "config", "lsp" }
+
+for _, dir in pairs(config_dirs) do
+  require_all_files(dir)
 end
 
 -- Load ColorScheme
 -- To set up additional color themes, install in plugins.lua and add a name
 --  lua config file in lua/colors and update the require statement below
 require("colors.onedark")
-
--- sumneko_lua Config
---
--- Make runtime files discoverable to the server
-local runtime_path = vim.split(package.path, ";")
-table.insert(runtime_path, "lua/?.lua")
-table.insert(runtime_path, "lua/?/init.lua")
-
-require("lspconfig").sumneko_lua.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
-  settings = {
-    Lua = {
-      runtime = {
-        -- Tell the language server which version of Lua you're using (most likely LuaJIT)
-        version = "LuaJIT",
-        -- Setup your lua path
-        path = runtime_path,
-      },
-      diagnostics = {
-        globals = { "vim" },
-      },
-      workspace = { library = vim.api.nvim_get_runtime_file("", true) },
-      -- Do not send telemetry data containing a randomized but unique identifier
-      telemetry = { enable = false },
-    },
-  },
-})
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
