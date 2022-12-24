f0b754d6174fc1179b36b9c9b36d345581585a5d
Shad Kaske <shadkaske@gmail.com>
2022-12-24 11:51:01 -0600

    Added Ctrl j/k to nvim-cmp selections

--- nvim/.config/nvim/lua/config/cmp.lua
+++ nvim/.config/nvim/lua/config/cmp.lua
@@ -42,6 +42,24 @@ cmp.setup({
         fallback()
       end
     end, { "i", "s" }),
+    ["<C-j>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+    ["<C-k>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
   }),
   sources = {
     { name = "nvim_lsp" },
