def034e586b422c847acdd8de508101e119d7a26
Shad Kaske <shadkaske@gmail.com>
2022-12-30 11:04:14 -0600

    Added bitwarden to awesome floating rules

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -812,6 +812,7 @@ awful.rules.rules = {
       "Wpa_gui",
       "veromix",
       "xtightvncviewer",
+      "Bitwarden",
       "VirtualBox Manager" },
 
     -- Note that the name property shown in xprop might be set slightly after creation of the client
