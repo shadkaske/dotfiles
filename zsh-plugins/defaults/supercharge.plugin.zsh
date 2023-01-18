fb6e2e848c3d18cc51f2d2aeb5d807054bb80ae4
Shad Kaske <shadkaske@gmail.com>
2023-01-18 16:11:50 -0600

    Changed some defaults

--- zsh-plugins/defaults/supercharge.plugin.zsh
+++ zsh-plugins/defaults/supercharge.plugin.zsh
@@ -16,7 +16,7 @@ unsetopt BEEP
 setopt AUTO_CD
 setopt GLOB_DOTS
 setopt NOMATCH
-setopt MENU_COMPLETE
+unsetopt MENU_COMPLETE
 setopt EXTENDED_GLOB
 setopt INTERACTIVE_COMMENTS
 setopt APPEND_HISTORY
@@ -26,8 +26,8 @@ HISTSIZE=50000
 SAVEHIST=50000
 setopt BANG_HIST                 # Treat the '!' character specially during expansion.
 setopt EXTENDED_HISTORY          # Write the history file in the ":start:elapsed;command" format.
-# setopt INC_APPEND_HISTORY        # Write to the history file immediately, not when the shell exits.
-# setopt SHARE_HISTORY             # Share history between all sessions.
+setopt INC_APPEND_HISTORY        # Write to the history file immediately, not when the shell exits.
+setopt SHARE_HISTORY             # Share history between all sessions.
 setopt HIST_EXPIRE_DUPS_FIRST    # Expire duplicate entries first when trimming history.
 setopt HIST_IGNORE_DUPS          # Don't record an entry that was just recorded again.
 setopt HIST_IGNORE_ALL_DUPS      # Delete old recorded entry if new entry is a duplicate.
@@ -48,5 +48,5 @@ autoload -Uz colors && colors
 # bindings
 bindkey -s '^x' '^usource ${ZDOTDIR:-$HOME}/.zshrc\nclear\n'
 bindkey -M menuselect '?' history-incremental-search-forward
-bindkey -M menuselect '/' history-incremental-search-backward
+# bindkey -M menuselect '/' history-incremental-search-backward
 
