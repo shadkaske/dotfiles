268162156ccf094fcbc04d2adc504b2f900e7269
Shad Kaske <shadkaske@gmail.com>
2023-12-11 06:18:21 -0600

    Aliases for fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,5 +1,16 @@
 set -U fish_greeting
 
+## Aliases ( Abbreviations )
+# lazygit
+abbr -a -- lg lazygit
+
+# Eza
+abbr -a -- la eza --icons --group --header --group-directories-first --long
+abbr -a -- ls eza --icons --group --header --group-directories-first
+
+## Fzf Settings
+# fzf_key_bindings
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
