182c69db0a6fc35969cad377a853fa913e9f2968
Shad Kaske <Shad.Kaske@showplacecabinetry.com>
2023-12-13 08:43:51 -0600

    Eza functions

---
+++ shell/.config/fish/functions/l.fish
@@ -0,0 +1,3 @@
+function l -d "List directory contents - short"
+    eza --icons --group --header --group-directories-first
+end
---
+++ shell/.config/fish/functions/la.fish
@@ -0,0 +1,4 @@
+function la -d "List all directory contents - long"
+    eza --icons --group --header --group-directories-first --long
+end
+
