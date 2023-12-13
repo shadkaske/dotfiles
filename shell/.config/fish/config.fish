5577e0dd5928585037b51cd756331e56a3431aaa
Shad Kaske <Shad.Kaske@showplacecabinetry.com>
2023-12-13 08:44:03 -0600

    Added fzf to path

--- shell/.config/fish/config.fish
+++ shell/.config/fish/config.fish
@@ -3,6 +3,7 @@ set -g fish_greeting
 fish_add_path $HOME/.cargo/bin/
 fish_add_path $HOME/.tmux/plugins/t-smart-tmux-session-manager/bin
 fish_add_path $HOME/.local/bin/
+fish_add_path $HOME/.fzf/bin/
 
 abbr --add cat bat
 abbr --add lg lazygit
