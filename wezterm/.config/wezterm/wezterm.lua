b8a122264b1fc86b99c0b4b7ffad489f38e72302
Shad Kaske <Shad.Kaske@showplacecabinetry.com>
2024-01-14 08:26:38 -0600

    Wezterm stuff

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,9 +1,7 @@
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
 
-local act = wezterm.action
-
-wezterm.on("update-right-status", function(window, pane)
+wezterm.on("update-right-status", function(window)
 	window:set_right_status(window:active_workspace())
 end)
 
@@ -22,23 +20,19 @@ config.initial_cols = 120
 -- Stop nagging me about closing
 config.window_close_confirmation = 'NeverPrompt'
 
--- Connect to Multiplexer as Default
-config.default_gui_startup_args = { "connect", "unix" }
-
 -- Spawn a fish shell in login mode
-config.default_prog = { '/usr/bin/fish', '-l' }
+config.default_prog = { '/bin/zsh', '-l' }
 
 -- ColorScheme
 config.color_scheme = "Catppuccin Mocha"
--- config.color_scheme = "OneDark (base16)"
 
 -- Disable Scrollbar
 config.enable_scroll_bar = false
 
 -- Fonts
-config.font = wezterm.font("JetBrains Mono", { weight = "Regular" })
+config.font = wezterm.font("JetBrains Mono", { weight = "ExtraLight" })
 
-config.font_size = 13
+config.font_size = 12
 
 -- Window Decorations
 -- config.window_decorations = "RESIZE"
@@ -58,222 +52,222 @@ config.window_padding = {
 config.enable_tab_bar = true
 config.use_fancy_tab_bar = false
 config.tab_bar_at_bottom = true
-config.hide_tab_bar_if_only_one_tab = false
+config.hide_tab_bar_if_only_one_tab = true
 
 -- SmartSplit
 
-local function is_vim(pane)
-	return pane:get_user_vars().IS_NVIM == "true"
-end
-
-local direction_keys = {
-	Left = "h",
-	Down = "j",
-	Up = "k",
-	Right = "l",
-	-- reverse lookup
-	h = "Left",
-	j = "Down",
-	k = "Up",
-	l = "Right",
-}
-
-local function split_nav(resize_or_move, key)
-	return {
-		key = key,
-		mods = resize_or_move == "resize" and "META" or "CTRL",
-		action = wezterm.action_callback(function(win, pane)
-			if is_vim(pane) then
-				-- pass the keys through to vim/nvim
-				win:perform_action({
-					SendKey = { key = key, mods = resize_or_move == "resize" and "META" or "CTRL" },
-				}, pane)
-			else
-				if resize_or_move == "resize" then
-					win:perform_action({ AdjustPaneSize = { direction_keys[key], 3 } }, pane)
-				else
-					win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
-				end
-			end
-		end),
-	}
-end
-
--- Keymaps
-config.keys = {
-	split_nav("move", "h"),
-	split_nav("move", "j"),
-	split_nav("move", "k"),
-	split_nav("move", "l"),
-	-- resize panes
-	split_nav("resize", "h"),
-	split_nav("resize", "j"),
-	split_nav("resize", "k"),
-	split_nav("resize", "l"),
-	-- New Workspace
-	{
-		key = "w",
-		mods = "ALT",
-		action = act.PromptInputLine({
-			description = wezterm.format({
-				{ Attribute = { Intensity = "Bold" } },
-				{ Foreground = { AnsiColor = "Fuchsia" } },
-				{ Text = "Enter name for new workspace" },
-			}),
-			action = wezterm.action_callback(function(window, pane, line)
-				-- line will be `nil` if they hit escape without entering anything
-				-- An empty string if they just hit enter
-				-- Or the actual line of text they wrote
-				if line then
-					window:perform_action(
-						act.SwitchToWorkspace({
-							name = line,
-                            -- spawn = {
-                            --     args = { "fzf-cd" },
-                            -- },
-						}),
-						pane
-					)
-				end
-			end),
-		}),
-	},
-	{
-		key = "t",
-		mods = "ALT",
-		action = act.SpawnTab("DefaultDomain"),
-	},
-	{
-		key = "Enter",
-		mods = "ALT",
-		action = act.SplitVertical({ domain = "CurrentPaneDomain" }),
-	},
-	{
-		key = "Enter",
-		mods = "ALT|CTRL",
-		action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }),
-	},
-	{
-		key = "L",
-		mods = "CTRL|SHIFT",
-		action = act.Multiple({
-			act.ClearScrollback("ScrollbackAndViewport"),
-			act.SendKey({ key = "L", mods = "CTRL" }),
-		}),
-	},
-	{
-		key = "e",
-		mods = "ALT",
-		action = act.PromptInputLine({
-			description = wezterm.format({
-				{ Attribute = { Intensity = "Bold" } },
-				{ Foreground = { AnsiColor = "Fuchsia" } },
-				{ Text = "Enter Tab Name: " },
-			}),
-			action = wezterm.action_callback(function(window, pane, line)
-				if line then
-					window:active_tab():set_title(line)
-				end
-			end),
-		}),
-	},
-	{
-		key = "z",
-		mods = "ALT",
-		action = act.TogglePaneZoomState,
-	},
-	{
-		key = "'",
-		mods = "ALT",
-		action = wezterm.action.ActivateLastTab,
-	},
-	{
-		key = "1",
-		mods = "ALT",
-		action = act.ActivateTab(0),
-	},
-	{
-		key = "2",
-		mods = "ALT",
-		action = act.ActivateTab(1),
-	},
-	{
-		key = "3",
-		mods = "ALT",
-		action = act.ActivateTab(2),
-	},
-	{
-		key = "4",
-		mods = "ALT",
-		action = act.ActivateTab(3),
-	},
-	{
-		key = "5",
-		mods = "ALT",
-		action = act.ActivateTab(4),
-	},
-	{
-		key = "6",
-		mods = "ALT",
-		action = act.ActivateTab(5),
-	},
-	{
-		key = "7",
-		mods = "ALT",
-		action = act.ActivateTab(6),
-	},
-	{
-		key = "8",
-		mods = "ALT",
-		action = act.ActivateTab(7),
-	},
-	{
-		key = "9",
-		mods = "ALT",
-		action = act.ActivateTab(8),
-	},
-	{
-		key = "p",
-		mods = "ALT",
-		action = act.ShowLauncherArgs({
-			flags = "FUZZY|WORKSPACES",
-		}),
-	},
-	{
-		key = "W",
-		mods = "CTRL|SHIFT",
-		action = act.ShowLauncherArgs({
-			flags = "FUZZY|WORKSPACES",
-		}),
-	},
-	{
-		key = "S",
-		mods = "CTRL|SHIFT",
-		action = act.ShowLauncherArgs({
-			flags = "FUZZY|DOMAINS",
-		}),
-	},
-    {
-        key = "G",
-        mods = "CTRL|SHIFT",
-        action = act.SpawnCommandInNewTab {
-            args = { 'lazygit' },
-        },
-    },
-    {
-        key = "E",
-        mods = "CTRL|SHIFT",
-        action = act.SpawnCommandInNewTab {
-            domain = 'CurrentPaneDomain',
-            args = { 'ranger' },
-        },
-    },
-    {
-        key = "q",
-        mods = "ALT",
-        action = act.CloseCurrentPane { confirm = false },
-    },
-}
+-- local function is_vim(pane)
+-- 	return pane:get_user_vars().IS_NVIM == "true"
+-- end
+--
+-- local direction_keys = {
+-- 	Left = "h",
+-- 	Down = "j",
+-- 	Up = "k",
+-- 	Right = "l",
+-- 	-- reverse lookup
+-- 	h = "Left",
+-- 	j = "Down",
+-- 	k = "Up",
+-- 	l = "Right",
+-- }
+--
+-- local function split_nav(resize_or_move, key)
+-- 	return {
+-- 		key = key,
+-- 		mods = resize_or_move == "resize" and "META" or "CTRL",
+-- 		action = wezterm.action_callback(function(win, pane)
+-- 			if is_vim(pane) then
+-- 				-- pass the keys through to vim/nvim
+-- 				win:perform_action({
+-- 					SendKey = { key = key, mods = resize_or_move == "resize" and "META" or "CTRL" },
+-- 				}, pane)
+-- 			else
+-- 				if resize_or_move == "resize" then
+-- 					win:perform_action({ AdjustPaneSize = { direction_keys[key], 3 } }, pane)
+-- 				else
+-- 					win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
+-- 				end
+-- 			end
+-- 		end),
+-- 	}
+-- end
+--
+-- -- Keymaps
+-- config.keys = {
+-- 	split_nav("move", "h"),
+-- 	split_nav("move", "j"),
+-- 	split_nav("move", "k"),
+-- 	split_nav("move", "l"),
+-- 	-- resize panes
+-- 	split_nav("resize", "h"),
+-- 	split_nav("resize", "j"),
+-- 	split_nav("resize", "k"),
+-- 	split_nav("resize", "l"),
+-- 	-- New Workspace
+-- 	{
+-- 		key = "w",
+-- 		mods = "ALT",
+-- 		action = act.PromptInputLine({
+-- 			description = wezterm.format({
+-- 				{ Attribute = { Intensity = "Bold" } },
+-- 				{ Foreground = { AnsiColor = "Fuchsia" } },
+-- 				{ Text = "Enter name for new workspace" },
+-- 			}),
+-- 			action = wezterm.action_callback(function(window, pane, line)
+-- 				-- line will be `nil` if they hit escape without entering anything
+-- 				-- An empty string if they just hit enter
+-- 				-- Or the actual line of text they wrote
+-- 				if line then
+-- 					window:perform_action(
+-- 						act.SwitchToWorkspace({
+-- 							name = line,
+--                             -- spawn = {
+--                             --     args = { "fzf-cd" },
+--                             -- },
+-- 						}),
+-- 						pane
+-- 					)
+-- 				end
+-- 			end),
+-- 		}),
+-- 	},
+-- 	{
+-- 		key = "t",
+-- 		mods = "ALT",
+-- 		action = act.SpawnTab("DefaultDomain"),
+-- 	},
+-- 	{
+-- 		key = "Enter",
+-- 		mods = "ALT",
+-- 		action = act.SplitVertical({ domain = "CurrentPaneDomain" }),
+-- 	},
+-- 	{
+-- 		key = "Enter",
+-- 		mods = "ALT|CTRL",
+-- 		action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+-- 	},
+-- 	{
+-- 		key = "L",
+-- 		mods = "CTRL|SHIFT",
+-- 		action = act.Multiple({
+-- 			act.ClearScrollback("ScrollbackAndViewport"),
+-- 			act.SendKey({ key = "L", mods = "CTRL" }),
+-- 		}),
+-- 	},
+-- 	{
+-- 		key = "e",
+-- 		mods = "ALT",
+-- 		action = act.PromptInputLine({
+-- 			description = wezterm.format({
+-- 				{ Attribute = { Intensity = "Bold" } },
+-- 				{ Foreground = { AnsiColor = "Fuchsia" } },
+-- 				{ Text = "Enter Tab Name: " },
+-- 			}),
+-- 			action = wezterm.action_callback(function(window, pane, line)
+-- 				if line then
+-- 					window:active_tab():set_title(line)
+-- 				end
+-- 			end),
+-- 		}),
+-- 	},
+-- 	{
+-- 		key = "z",
+-- 		mods = "ALT",
+-- 		action = act.TogglePaneZoomState,
+-- 	},
+-- 	{
+-- 		key = "'",
+-- 		mods = "ALT",
+-- 		action = wezterm.action.ActivateLastTab,
+-- 	},
+-- 	{
+-- 		key = "1",
+-- 		mods = "ALT",
+-- 		action = act.ActivateTab(0),
+-- 	},
+-- 	{
+-- 		key = "2",
+-- 		mods = "ALT",
+-- 		action = act.ActivateTab(1),
+-- 	},
+-- 	{
+-- 		key = "3",
+-- 		mods = "ALT",
+-- 		action = act.ActivateTab(2),
+-- 	},
+-- 	{
+-- 		key = "4",
+-- 		mods = "ALT",
+-- 		action = act.ActivateTab(3),
+-- 	},
+-- 	{
+-- 		key = "5",
+-- 		mods = "ALT",
+-- 		action = act.ActivateTab(4),
+-- 	},
+-- 	{
+-- 		key = "6",
+-- 		mods = "ALT",
+-- 		action = act.ActivateTab(5),
+-- 	},
+-- 	{
+-- 		key = "7",
+-- 		mods = "ALT",
+-- 		action = act.ActivateTab(6),
+-- 	},
+-- 	{
+-- 		key = "8",
+-- 		mods = "ALT",
+-- 		action = act.ActivateTab(7),
+-- 	},
+-- 	{
+-- 		key = "9",
+-- 		mods = "ALT",
+-- 		action = act.ActivateTab(8),
+-- 	},
+-- 	{
+-- 		key = "p",
+-- 		mods = "ALT",
+-- 		action = act.ShowLauncherArgs({
+-- 			flags = "FUZZY|WORKSPACES",
+-- 		}),
+-- 	},
+-- 	{
+-- 		key = "W",
+-- 		mods = "CTRL|SHIFT",
+-- 		action = act.ShowLauncherArgs({
+-- 			flags = "FUZZY|WORKSPACES",
+-- 		}),
+-- 	},
+-- 	{
+-- 		key = "S",
+-- 		mods = "CTRL|SHIFT",
+-- 		action = act.ShowLauncherArgs({
+-- 			flags = "FUZZY|DOMAINS",
+-- 		}),
+-- 	},
+--     {
+--         key = "G",
+--         mods = "CTRL|SHIFT",
+--         action = act.SpawnCommandInNewTab {
+--             args = { 'lazygit' },
+--         },
+--     },
+--     {
+--         key = "E",
+--         mods = "CTRL|SHIFT",
+--         action = act.SpawnCommandInNewTab {
+--             domain = 'CurrentPaneDomain',
+--             args = { 'ranger' },
+--         },
+--     },
+--     {
+--         key = "q",
+--         mods = "ALT",
+--         action = act.CloseCurrentPane { confirm = false },
+--     },
+-- }
 
 -- and finally, return the configuration to wezterm
 return config
