e975d8d94b375608bb281baa74bb9dfc6e8f9560
Shad Kaske <shadkaske@gmail.com>
2024-05-17 10:29:46 -0500

    Set dark theme for legacy and other

--- gsettings.sh
+++ gsettings.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+gsettings set org.gnome.desktop.interface color-scheme "'prefer-dark'"
+
+gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3-dark'
 
 gsettings set org.gnome.mutter center-new-windows true
 
@@ -9,9 +12,9 @@ gsettings set org.gnome.mutter dynamic-workspaces false
 gsettings set org.gnome.desktop.wm.preferences num-workspaces 9
 
 for i in {1..9}; do
-    gsettings set "org.gnome.shell.keybindings" "switch-to-application-$i" "[]"
-    gsettings set "org.gnome.desktop.wm.keybindings" "move-to-workspace-$i" "['<Shift><Super>$i']"
-    gsettings set "org.gnome.desktop.wm.keybindings" "switch-to-workspace-$i" "['<Super>$i']"
+	gsettings set "org.gnome.shell.keybindings" "switch-to-application-$i" "[]"
+	gsettings set "org.gnome.desktop.wm.keybindings" "move-to-workspace-$i" "['<Shift><Super>$i']"
+	gsettings set "org.gnome.desktop.wm.keybindings" "switch-to-workspace-$i" "['<Super>$i']"
 done
 
 # DD Term Config
