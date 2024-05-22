4fd17fc826b52df9b385d8418ccea8777684aa32
Shad Kaske <shadkaske@gmail.com>
2024-05-22 15:53:36 -0500

    Sesh config updates

--- sesh/.config/sesh/scripts/laravel-watch.sh
+++ sesh/.config/sesh/scripts/laravel-watch.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 tmux rename-window 'Editor'
 tmux new-window -c '#{pane_current_path}' -n 'Servers' yarn run watch
-tmux split-window -v -p 50 "php artisan serve"
-# tmux new-window -c '#{pane_current_path}' -n 'git' lazygit
+tmux split-window -c '#{pane_current_path}' -v "php artisan serve"
 tmux select-window -t 1
-nvim -c ':Telescope find_files'
+nvim
