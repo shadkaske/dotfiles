0d997a58392585c90ed52ae0d9240cdd9b9e81ff
Shad Kaske <shadkaske@gmail.com>
2024-06-05 11:21:23 -0500

    Stop opening telescope on sesh

--- sesh/.config/sesh/scripts/laravel.sh
+++ sesh/.config/sesh/scripts/laravel.sh
@@ -3,4 +3,4 @@ tmux rename-window 'Editor'
 tmux new-window -c '#{pane_current_path}' -n 'Servers' yarn run dev
 tmux split-window -c '#{pane_current_path}' -v "php artisan serve"
 tmux select-window -t 1
-nvim -c ':Telescope find_files'
+nvim
