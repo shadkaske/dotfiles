b975a41fd8c9db90364a58580823bcf4a645dca8
Shad Kaske <shadkaske@gmail.com>
2024-07-19 06:18:32 -0500

    Lazy load mini-pairs

--- nvim/.config/nvim/lua/plugins/mini-pairs.lua
+++ nvim/.config/nvim/lua/plugins/mini-pairs.lua
@@ -1,7 +1,6 @@
 return {
-  "echasnovski/mini.pairs",
-  version = "*",
-  config = function ()
-    require("mini.pairs").setup()
-  end,
+  'echasnovski/mini.pairs',
+  version = '*',
+  event = 'VeryLazy',
+  opts = {},
 }
