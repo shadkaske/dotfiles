44250f1869ec91a3870925444f4e21879f29ef40
Shad Kaske <shadkaske@gmail.com>
2024-07-18 22:48:06 -0500

    lazygit toggle term added

--- nvim/.config/nvim/lua/util/init.lua
+++ nvim/.config/nvim/lua/util/init.lua
@@ -2,13 +2,13 @@
 local M = {}
 
 function M.plugin_loaded(plugin)
-  local lazy_config_avail, lazy_config = pcall(require, "lazy.core.config")
+  local lazy_config_avail, lazy_config = pcall(require, 'lazy.core.config')
   return lazy_config_avail and lazy_config.plugins[plugin] ~= nil
 end
 
 ---@param on_attach fun(client, buffer)
 function M.on_attach(on_attach)
-  vim.api.nvim_create_autocmd("LspAttach", {
+  vim.api.nvim_create_autocmd('LspAttach', {
     callback = function(args)
       local buffer = args.buf
       local client = vim.lsp.get_client_by_id(args.data.client_id)
@@ -18,40 +18,52 @@ function M.on_attach(on_attach)
 end
 
 function M.toggle_tinker()
-  local Terminal = require("toggleterm.terminal").Terminal
-  local tinker = Terminal:new({
-    cmd = "php artisan tinker",
-    dir = "git_dir",
-    direction = "horizontal",
+  local Terminal = require('toggleterm.terminal').Terminal
+  local tinker = Terminal:new {
+    cmd = 'php artisan tinker',
+    dir = 'git_dir',
+    direction = 'horizontal',
     size = 50,
-    hidden = true
-  })
+    hidden = true,
+  }
 
   tinker:toggle()
 end
 
 function M.toggle_yarn_watch()
-  local Terminal = require("toggleterm.terminal").Terminal
-  local watch = Terminal:new({
-    cmd = "yarn run watch",
-    dir = "git_dir",
-    direction = "horizontal",
+  local Terminal = require('toggleterm.terminal').Terminal
+  local watch = Terminal:new {
+    cmd = 'yarn run watch',
+    dir = 'git_dir',
+    direction = 'horizontal',
     size = 50,
-    hidden = true
-  })
+    hidden = true,
+  }
 
   watch:toggle()
 end
 
 function M.toggle_yarn_dev()
-  local Terminal = require("toggleterm.terminal").Terminal
-  local watch = Terminal:new({
-    cmd = "yarn run dev",
-    dir = "git_dir",
-    direction = "horizontal",
+  local Terminal = require('toggleterm.terminal').Terminal
+  local watch = Terminal:new {
+    cmd = 'yarn run dev',
+    dir = 'git_dir',
+    direction = 'horizontal',
     size = 50,
-    hidden = true
-  })
+    hidden = true,
+  }
+
+  watch:toggle()
+end
+
+function M.toggle_lazygit()
+  local Terminal = require('toggleterm.terminal').Terminal
+  local watch = Terminal:new {
+    cmd = 'lazygit',
+    dir = 'git_dir',
+    direction = 'float',
+    hidden = true,
+  }
 
   watch:toggle()
 end
