4caceb07b04d1a5d535c79a6d1dd395f62aae234
Shad Kaske <shadkaske@gmail.com>
2024-07-19 21:03:28 -0500

    Changed oil.nvim vertical split keys

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -5,15 +5,11 @@ return {
   opts = {
     default_file_explorer = true,
     skip_confirm_for_simple_edits = true,
-    columns = {
-      'icon',
-      -- "permissions",
-      'size',
-      'mtime',
-    },
     keymaps = {
       ['<C-h>'] = false,
       ['<C-x'] = { 'actions.select', opts = { horizontal = true }, desc = 'Open the entry in a horizontal split' },
+      ['<C-s>'] = false,
+      ['<C-v'] = { 'actions.select', opts = { horizontal = true }, desc = 'Open the entry in a horizontal split' },
     },
   },
   dependencies = { 'nvim-tree/nvim-web-devicons' },
