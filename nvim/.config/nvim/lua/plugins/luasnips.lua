5e2a5434b1928ceafc6b2fd73f14e37ae703b5e1
Shad Kaske <shadkaske@gmail.com>
2024-07-21 09:23:28 -0500

    Lazy Load LuaSnips, Cleaned Up Config

--- nvim/.config/nvim/lua/plugins/luasnips.lua
+++ nvim/.config/nvim/lua/plugins/luasnips.lua
@@ -2,41 +2,21 @@ return {
   'L3MON4D3/LuaSnip',
   version = 'v2.*',
   build = 'make install_jsregexp',
-  event = 'BufReadPre',
-  opt = {},
+  lazy = true,
+  dependencies = {
+    {
+      'rafamadriz/friendly-snippets',
+      config = function()
+        require('luasnip.loaders.from_vscode').lazy_load()
+      end,
+    },
+  },
+  opts = {
+    history = true,
+    delete_check_events = 'TextChanged',
+    updateevents = 'TextChanged, TextChangedI',
+  },
   init = function()
     require('luasnip.loaders.from_lua').load './luasnippets'
-  end,
-  config = function()
-    local ls = require 'luasnip'
-    local types = require 'luasnip.util.types'
-
-    ls.config.set_config {
-      history = true,
-      updateevents = 'TextChanged, TextChangedI',
-      ext_opts = {
-        [types.choiceNode] = {
-          active = {
-            virt_text = { { '<-', 'Choice' } },
-          },
-        },
-      },
-    }
-
-    vim.keymap.set({ 'i' }, '<C-Y>', function()
-      ls.expand()
-    end, { silent = true })
-    vim.keymap.set({ 'i', 's' }, '<C-L>', function()
-      ls.jump(1)
-    end, { silent = true })
-    vim.keymap.set({ 'i', 's' }, '<C-H>', function()
-      ls.jump(-1)
-    end, { silent = true })
-
-    vim.keymap.set({ 'i', 's' }, '<C-E>', function()
-      if ls.choice_active() then
-        ls.change_choice(1)
-      end
-    end, { silent = true })
   end,
 }
