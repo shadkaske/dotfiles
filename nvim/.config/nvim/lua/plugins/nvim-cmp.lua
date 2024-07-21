1222f75a42889b8ca99e8bb5e1d48c642af2a222
Shad Kaske <shadkaske@gmail.com>
2024-07-21 07:42:18 -0500

    Added additional luasnip keys to nvim-cmp

--- nvim/.config/nvim/lua/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -100,6 +100,20 @@ return {
           behavior = cmp.ConfirmBehavior.Replace,
           select = true,
         },
+        ['<C-l>'] = cmp.mapping(function(fallback)
+          if luasnip.expand_or_locally_jumpable() then
+            luasnip.expand_or_jump()
+          else
+            fallback()
+          end
+        end, { 'i', 's' }),
+        ['<C-h>'] = cmp.mapping(function(fallback)
+          if luasnip.locally_jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { 'i', 's' }),
         ['<Tab>'] = cmp.mapping(function(fallback)
           if luasnip.expand_or_locally_jumpable() then
             luasnip.expand_or_jump()
