ed922df75852b100c75a9896082e2bd4d966ae79
Shad Kaske <shadkaske@gmail.com>
2024-07-23 06:44:50 -0500

    Updates to laravel plugin. Messy way to update the ide-helper

--- nvim/.config/nvim/lua/plugins/laravel.lua
+++ nvim/.config/nvim/lua/plugins/laravel.lua
@@ -8,17 +8,30 @@ return {
   },
   cmd = { 'Artisan', 'Composer', 'Npm', 'Yarn', 'Laravel' },
   event = { 'VeryLazy' },
-  features = {
-    null_ls = {
-      enable = true,
-    },
-  },
   keys = {
     { '<leader>la', ':Laravel artisan<cr>', desc = 'Artisan Commands' },
-    { '<leader>lr', ':Laravel routes<cr>', desc = 'Laravel Routes' },
+    { '<leader>lr', ':Laravel routes<cr>',  desc = 'Laravel Routes' },
+    {
+      '<leader>lb',
+      function()
+        local run = require "laravel.run"
+        run("artisan", { "ide-helper:meta --no-interaction --quiet" }, {})
+        run("artisan", { "ide-helper:eloquent --no-interaction --quiet" }, {})
+        run("artisan", { "ide-helper:generate --no-interaction --quiet" }, {})
+        run("artisan", { "ide-helper:models --no-interaction --nowrite --quiet" }, {})
+      end,
+      desc = 'Build Ide Helper'
+    },
   },
   config = function()
-    require('laravel').setup()
+    require('laravel').setup({
+      lsp_server = "intelephense",
+      features = {
+        null_ls = {
+          enable = false,
+        },
+      }
+    })
     require('telescope').load_extension 'laravel'
   end,
 }
