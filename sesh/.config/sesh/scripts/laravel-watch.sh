a8c3faa75837fe42d3b13c1256eea1db775b454e
Shad Kaske <shadkaske@gmail.com>
2024-09-30 09:06:39 -0500

    Default Params for sesh scripts to open dev environment

--- sesh/.config/sesh/scripts/laravel-watch.sh
+++ sesh/.config/sesh/scripts/laravel-watch.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 tmux rename-window 'Editor'
 tmux new-window -c '#{pane_current_path}' -n 'Servers' yarn run watch
-# tmux split-window -c '#{pane_current_path}' -v "php artisan serve"
+tmux split-window -c '#{pane_current_path}' -v "php artisan serve"
 tmux select-window -t 1
 nvim
