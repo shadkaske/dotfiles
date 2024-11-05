20bbebee6d73d9c032b5adf30848d0b87e8891c0
Shad Kaske <shadkaske@gmail.com>
2024-11-05 12:59:50 -0500

    Hyperland updates adn wlpaste

--- nvim/.config/nvim/lua/plugins/leap.lua
+++ nvim/.config/nvim/lua/plugins/leap.lua
@@ -19,14 +19,14 @@ return {
         "tpope/vim-repeat",
       },
     },
-    -- keys = function()
-    --   ---@type LazyKeys[]
-    --   local ret = {}
-    --   for _, key in ipairs { "f", "F", "t", "T" } do
-    --     ret[#ret + 1] = { key, mode = { "n", "x", "o" }, desc = key }
-    --   end
-    --   return ret
-    -- end,
-    -- opts = { labeled_modes = "nx" },
+    keys = function()
+      ---@type LazyKeys[]
+      local ret = {}
+      for _, key in ipairs { "f", "F", "t", "T" } do
+        ret[#ret + 1] = { key, mode = { "n", "x", "o" }, desc = key }
+      end
+      return ret
+    end,
+    opts = { labeled_modes = "nx" },
   },
 }
