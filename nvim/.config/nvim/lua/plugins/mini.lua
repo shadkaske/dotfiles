2a2cf81ac3b113a5801a1a51a73b0cd6aaed7b0f
Shad Kaske <shadkaske@gmail.com>
2024-12-20 06:09:59 -0600

    Moving mini plugins to single config

---
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -0,0 +1,29 @@
+return {
+  'echasnovski/mini.nvim',
+  version = false,
+  lazy = false,
+  opts = {},
+  config = function()
+    require('mini.icons').setup()
+    require('mini.ai').setup()
+    require('mini.diff').setup()
+    require('mini.pairs').setup()
+    require('mini.files').setup()
+    require('mini.git').setup()
+    require('mini.surround').setup({
+      mappings = {
+        add = 'gza',            -- Add surrounding in Normal and Visual modes
+        delete = 'gzd',         -- Delete surrounding
+        find = 'gzf',           -- Find surrounding (to the right)
+        find_left = 'gzF',      -- Find surrounding (to the left)
+        highlight = 'gzh',      -- Highlight surrounding
+        replace = 'gzr',        -- Replace surrounding
+        update_n_lines = 'gzn', -- Update `n_lines`
+      },
+    })
+    vim.keymap.set('n', '-', function() MiniFiles.open(vim.api.nvim_buf_get_name(0)) end,
+      { desc = 'Explore Current Buffers Directory' })
+    vim.keymap.set('n', '<leader>e', function() MiniFiles.open(nil, false) end,
+      { desc = 'Explore Current Working Directory' })
+  end,
+}
