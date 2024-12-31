59ed5c0e19dcba6faa11898d5d1e1ab82d0743ab
Shad Kaske <shadkaske@gmail.com>
2024-12-31 08:24:08 -0600

    Modified sesh startup scripts to remove window names

--- sesh/.config/sesh/scripts/laravel-mailpit.sh
+++ sesh/.config/sesh/scripts/laravel-mailpit.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 ARG1=${1:-localhost}
-tmux rename-window 'Editor'
-tmux new-window -c '#{pane_current_path}' -n 'Servers' yarn run dev --host=$ARG1.test
+tmux new-window -c '#{pane_current_path}' yarn run dev --host=$ARG1.test
 tmux split-window -c '#{pane_current_path}' -v "php artisan serve --host=$ARG1.test"
 tmux split-window -c '#{pane_current_path}' -v "mailpit"
 tmux split-window -c '#{pane_current_path}' -v "php artisan queue:listen"
--- sesh/.config/sesh/scripts/laravel.sh
+++ sesh/.config/sesh/scripts/laravel.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 ARG1=${1:-localhost}
-tmux rename-window 'Editor'
-tmux new-window -c '#{pane_current_path}' -n 'Servers' yarn run dev --host=localhost
+tmux new-window -c '#{pane_current_path}' yarn run dev --host=localhost
 tmux split-window -c '#{pane_current_path}' -v php artisan serve --host=localhost
 tmux select-window -t 1
 nvim
