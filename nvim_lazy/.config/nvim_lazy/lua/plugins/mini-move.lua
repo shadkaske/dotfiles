d90612dcdcf048f9cf4fe116ade04b12686dd5bb
Shad Kaske <shadkaske@gmail.com>
2025-01-21 10:50:59 -0600

    Additions to lazyvim config

---
+++ nvim_lazy/.config/nvim_lazy/lua/plugins/mini-move.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "echasnovski/mini.move",
+    event = "VeryLazy",
+    opts = {
+      mappings = {
+        left = "<M-S-h>",
+        right = "<M-S-l>",
+        down = "<M-S-j>",
+        up = "<M-S-k>",
+
+        line_left = "<M-S-h>",
+        line_right = "<M-S-l>",
+        line_down = "<M-S-j>",
+        line_up = "<M-S-k>",
+      },
+    },
+  },
+}
