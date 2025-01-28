072a651081c49db46244f38cc3d3b1f0add0b08e
Shad Kaske <shadkaske@gmail.com>
2025-01-28 13:40:12 -0600

    Added autocommand to change mini.jump highlight

--- nvim/.config/nvim/lua/config/autocommands.lua
+++ nvim/.config/nvim/lua/config/autocommands.lua
@@ -1,44 +1,56 @@
 -- Autocommands here
 -- [[ Highlight on yank ]]
-local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
-vim.api.nvim_create_autocmd('TextYankPost', {
+local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
+vim.api.nvim_create_autocmd("TextYankPost", {
   callback = function()
     vim.highlight.on_yank()
   end,
   group = highlight_group,
-  pattern = '*',
+  pattern = "*",
+})
+
+-- Change some default highlight_group settings
+vim.api.nvim_create_autocmd({ "ColorScheme", "VimEnter" }, {
+  group = vim.api.nvim_create_augroup("Color", {}),
+  pattern = "*",
+  callback = function()
+    vim.api.nvim_set_hl(0, "MiniJump", { bg = "#545c7e" })
+  end,
 })
 
 -- Automagically Trim Whitespace
-vim.api.nvim_create_autocmd({ 'BufWritePre' }, { pattern = { '*' }, command = [[%s/\s\+$//e]] })
+vim.api.nvim_create_autocmd({ "BufWritePre" }, { pattern = { "*" }, command = [[%s/\s\+$//e]] })
 
 -- Turn of diagnostics for .env files
-vim.api.nvim_create_autocmd({ 'BufEnter' }, {
-  pattern = { '.env', '.env.example' },
+vim.api.nvim_create_autocmd({ "BufEnter" }, {
+  pattern = { ".env", ".env.example" },
   command = [[lua vim.diagnostic.disable()]],
 })
 
 -- Set Btxml Files to xml filetype
-vim.api.nvim_create_autocmd({ 'BufWritePre', 'BufNewFile' }, { pattern = { '*.btxml' }, command = [[set filetype=xml]] })
+vim.api.nvim_create_autocmd(
+  { "BufWritePre", "BufNewFile" },
+  { pattern = { "*.btxml" }, command = [[set filetype=xml]] }
+)
 
 -- Set Lsp Mappings
-vim.api.nvim_create_autocmd('LspAttach', {
-  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("UserLspConfig", {}),
   callback = function(ev)
     -- Enable completion triggered by <c-x><c-o>
-    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+    vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 
     -- Buffer local mappings.
     -- See `:help vim.lsp.*` for documentation on any of the below functions
     local opts = { buffer = ev.buf }
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
     -- vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-    vim.keymap.set('n', '<space>cD', vim.lsp.buf.type_definition, { desc = 'Type Definition' })
-    vim.keymap.set('n', '<space>cr', vim.lsp.buf.rename, opts)
-    vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set("n", "<space>cD", vim.lsp.buf.type_definition, { desc = "Type Definition" })
+    vim.keymap.set("n", "<space>cr", vim.lsp.buf.rename, opts)
+    vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
+    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
   end,
 })
