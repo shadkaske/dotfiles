211495320c50787f126e6ca39e75f3aa063b684a
Shad Kaske <shadkaske@gmail.com>
2025-02-05 07:47:18 -0600

    Sesh config updates

--- sesh/.config/sesh/scripts/laravel-watch.sh
+++ sesh/.config/sesh/scripts/laravel-watch.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-tmux rename-window 'Editor'
-tmux new-window -c '#{pane_current_path}' -n 'Servers' 'php artisan serve --host=localhost'
+tmux new-window -c '#{pane_current_path}' 'php artisan serve --host=localhost'
 tmux split-window -c '#{pane_current_path}' -v yarn run watch
 tmux split-window -c '#{pane_current_path}' -h php artisan pail
 tmux select-window -t 1
