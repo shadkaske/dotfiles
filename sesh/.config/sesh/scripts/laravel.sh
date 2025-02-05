5c6f9346c5acb19504f691fc069719747d58bdcd
Shad Kaske <shadkaske@gmail.com>
2025-02-05 07:46:54 -0600

    Rofi is working with Plasma again

--- sesh/.config/sesh/scripts/laravel.sh
+++ sesh/.config/sesh/scripts/laravel.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 ARG1=${1:-localhost}
-tmux new-window -c '#{pane_current_path}' yarn run dev --host=localhost
-tmux split-window -c '#{pane_current_path}' -v php artisan serve --host=localhost
+tmux new-window -c '#{pane_current_path}' php artisan serve --host=localhost
+tmux split-window -c '#{pane_current_path}' -v yarn run dev --host=localhost
 tmux select-window -t 1
 nvim
