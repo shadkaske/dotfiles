c19d97a682712a2346e235f08d1628aff37b361e
Shad Kaske <shadkaske@gmail.com>
2025-02-06 14:31:42 -0600

    Updated larvel sesh script to use composer run dv

--- sesh/.config/sesh/scripts/laravel.sh
+++ sesh/.config/sesh/scripts/laravel.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 ARG1=${1:-localhost}
-tmux new-window -c '#{pane_current_path}' php artisan serve --host=localhost
-tmux split-window -c '#{pane_current_path}' -v yarn run dev --host=localhost
+tmux new-window -c '#{pane_current_path}' composer run dev
 tmux select-window -t 1
 nvim
