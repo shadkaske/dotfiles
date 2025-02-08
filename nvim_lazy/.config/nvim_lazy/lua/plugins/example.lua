9ba134e4d3e84cba12b0b99e2b9e82d6cfe33c0c
Shad Kaske <shadkaske@gmail.com>
2025-02-08 14:27:25 -0600

    LazyVim - Added mini files

--- nvim_lazy/.config/nvim_lazy/lua/plugins/example.lua
+++ nvim_lazy/.config/nvim_lazy/lua/plugins/example.lua
@@ -34,7 +34,6 @@ return {
   {
     "hrsh7th/nvim-cmp",
     dependencies = { "hrsh7th/cmp-emoji" },
-    ---@param opts cmp.ConfigSchema
     opts = function(_, opts)
       table.insert(opts.sources, { name = "emoji" })
     end,
@@ -68,7 +67,6 @@ return {
     "neovim/nvim-lspconfig",
     ---@class PluginLspOpts
     opts = {
-      ---@type lspconfig.options
       servers = {
         -- pyright will be automatically installed with mason and loaded with lspconfig
         pyright = {},
@@ -91,14 +89,12 @@ return {
     },
     ---@class PluginLspOpts
     opts = {
-      ---@type lspconfig.options
       servers = {
         -- tsserver will be automatically installed with mason and loaded with lspconfig
         tsserver = {},
       },
       -- you can do any additional lsp server setup here
       -- return true if you don't want this server to be setup with lspconfig
-      ---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
       setup = {
         -- example to setup with typescript.nvim
         tsserver = function(_, opts)
