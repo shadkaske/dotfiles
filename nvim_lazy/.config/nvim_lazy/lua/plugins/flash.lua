a28a387748ff47302fe5c5d9387db4a35bc95609
Shad Kaske <shadkaske@gmail.com>
2025-02-08 15:38:22 -0600

    LazyVim - Changed FlashLabel hlgroup

---
+++ nvim_lazy/.config/nvim_lazy/lua/plugins/flash.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "folke/flash.nvim",
+    opts = {
+      modes = {
+        char = {
+          highlight = {
+            groups = {
+              label = "IncSearch",
+            },
+          },
+        },
+      },
+    },
+  },
+}
