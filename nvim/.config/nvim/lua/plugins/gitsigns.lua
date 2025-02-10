2bf5b582c12e475a6d2e8a2a23addd898a7a3977
Shad Kaske <shadkaske@gmail.com>
2025-02-10 11:00:23 -0600

    Update gitsigns remove mini.git and mini.diff

--- nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -1,18 +1,59 @@
 return {
-  -- Adds git releated signs to the gutter, as well as utilities for managing changes
-  'lewis6991/gitsigns.nvim',
-  lazy = false,
-  opts = {
-    signcolumn = false,
-  },
-  keys = {
-    { '<leader>gP', "<cmd>lua require 'gitsigns'.preview_hunk()<cr>",                           desc = 'Preview Hunk' },
-    { '<leader>gR', "<cmd>lua require 'gitsigns'.reset_buffer()<cr>",                           desc = 'Reset Buffer' },
-    { '<leader>gd', '<cmd>Gitsigns diffthis HEAD<cr>',                                          desc = 'Git Diff' },
-    { '<leader>gn', "<cmd>lua require('gitsigns').next_hunk({navigation_message=false})<cr>zz", desc = 'Next Hunk' },
-    { '<leader>gp', "<cmd>lua require('gitsigns').prev_hunk({navigation_message=false})<cr>zz", desc = 'Previous Hunk' },
-    { '<leader>gr', "<cmd>lua require 'gitsigns'.reset_hunk()<cr>",                             desc = 'Reset Hunk' },
-    { '<leader>gs', "<cmd>lua require 'gitsigns'.stage_hunk()<cr>",                             desc = 'Stage Hunk' },
-    { '<leader>gu', "<cmd>lua require 'gitsigns'.undo_stage_hunk()<cr>",                        desc = 'Undo Stage Hunk' },
+  {
+    "lewis6991/gitsigns.nvim",
+    event = { "BufEnter" },
+    opts = {
+      signs = {
+        add = { text = "▎" },
+        change = { text = "▎" },
+        delete = { text = "" },
+        topdelete = { text = "" },
+        changedelete = { text = "▎" },
+        untracked = { text = "▎" },
+      },
+      signs_staged = {
+        add = { text = "▎" },
+        change = { text = "▎" },
+        delete = { text = "" },
+        topdelete = { text = "" },
+        changedelete = { text = "▎" },
+      },
+      on_attach = function(buffer)
+        local gs = package.loaded.gitsigns
+
+        local function map(mode, l, r, desc)
+          vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
+        end
+
+      -- stylua: ignore start
+      map("n", "]h", function()
+        if vim.wo.diff then
+          vim.cmd.normal({ "]c", bang = true })
+        else
+          gs.nav_hunk("next")
+        end
+      end, "Next Hunk")
+      map("n", "[h", function()
+        if vim.wo.diff then
+          vim.cmd.normal({ "[c", bang = true })
+        else
+          gs.nav_hunk("prev")
+        end
+      end, "Prev Hunk")
+      map("n", "]H", function() gs.nav_hunk("last") end, "Last Hunk")
+      map("n", "[H", function() gs.nav_hunk("first") end, "First Hunk")
+      map({ "n", "v" }, "<leader>ghs", ":Gitsigns stage_hunk<CR>", "Stage Hunk")
+      map({ "n", "v" }, "<leader>ghr", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
+      map("n", "<leader>ghS", gs.stage_buffer, "Stage Buffer")
+      map("n", "<leader>ghu", gs.undo_stage_hunk, "Undo Stage Hunk")
+      map("n", "<leader>ghR", gs.reset_buffer, "Reset Buffer")
+      map("n", "<leader>ghp", gs.preview_hunk_inline, "Preview Hunk Inline")
+      map("n", "<leader>ghb", function() gs.blame_line({ full = true }) end, "Blame Line")
+      map("n", "<leader>ghB", function() gs.blame() end, "Blame Buffer")
+      map("n", "<leader>ghd", gs.diffthis, "Diff This")
+      map("n", "<leader>ghD", function() gs.diffthis("~") end, "Diff This ~")
+      map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
+      end,
+    },
   },
 }
