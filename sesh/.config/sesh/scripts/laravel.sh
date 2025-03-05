e3ccc46829767781435edebafac64d6cc306a7ed
Shad Kaske <shadkaske@gmail.com>
2025-03-05 15:47:43 -0600

    Sesh Scripts for Laravel Updated

--- sesh/.config/sesh/scripts/laravel.sh
+++ sesh/.config/sesh/scripts/laravel.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 ARG1=${1:-localhost}
-tmux new-window -c '#{pane_current_path}' composer run dev
+tmux new-window -c '#{pane_current_path}' 'php artisan serve --host=localhost'
+tmux split-window -c '#{pane_current_path}' -v yarn run dev
+tmux split-window -c '#{pane_current_path}' -h 'php artisan pail --timeout=0'
 tmux select-window -t 1
 nvim
