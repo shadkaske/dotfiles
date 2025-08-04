cf59ab9630bfeaa53c6099f8a6bb5de70cf1d249
Shad Kaske <shadkaske@gmail.com>
2025-08-04 10:31:00 -0500

    Wezterm Config is Back

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,32 @@
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+config.font = wezterm.font("JetBrainsMono NFM Thin")
+
+config.font_size = 13
+
+config.color_scheme = "Tokyo Night"
+
+config.hide_tab_bar_if_only_one_tab = true
+
+config.window_decorations = "RESIZE"
+
+config.keys = {
+	{ key = "Enter", mods = "ALT", action = wezterm.action.DisableDefaultAssignment },
+}
+
+config.skip_close_confirmation_for_processes_named = {
+	"bash",
+	"sh",
+	"zsh",
+	"fish",
+	"tmux",
+	"nu",
+	"cmd.exe",
+	"pwsh.exe",
+	"powershell.exe",
+	"zellij",
+}
+
+return config
