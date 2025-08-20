0249fb73352256afd629c4c4e5219e4fb8a63108
Shad Kaske <shadkaske@gmail.com>
2025-08-20 15:20:54 -0500

    Sesh Settings Changes

--- sesh/.config/sesh/scripts/laravel-sail-mix.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-tmux new-window -c '#{pane_current_path}' './vendor/bin/sail down && ./vendor/bin/sail up'
-sleep 2
-tmux split-window -c '#{pane_current_path}' -v ./vendor/bin/sail yarn run watch-poll
-tmux select-window -t 1
-nvim
