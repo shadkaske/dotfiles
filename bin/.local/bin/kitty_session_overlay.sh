55cca39ec5d04f05c10c09edae316ad0a0ccbafd
Shad Kaske <shadkaske@gmail.com>
2025-09-24 14:52:09 -0500

    Need my persistent Tmux Session, ditching Kitty Multiplexing again

--- bin/.local/bin/kitty_session_overlay.sh
+++ bin/.local/bin/kitty_session_overlay.sh
@@ -5,7 +5,7 @@ sessions_dir="$HOME/.config/kitty/sessions/"
 select_session="$(ls $sessions_dir | fzf --border-label='Kitty Session Picker')"
 
 if [[ -n $select_session ]]; then
-  nohup $(which kitty) --session="$sessions_dir$select_session" >/dev/null 2>/dev/null &
+  nohup "$(which kitty)" --session="$sessions_dir$select_session" >/dev/null 2>/dev/null &
   disown
   exit
 fi
