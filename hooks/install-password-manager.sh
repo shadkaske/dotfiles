b960de751229ab3e4a49973867507bcd35007846
Shad Kaske <shadkaske@gmail.com>
2025-11-01 11:20:48 -0500

    Changing password manager to bitwarden cli

--- hooks/install-password-manager.sh
+++ hooks/install-password-manager.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 # exit immediately if password-manager-binary is already in $PATH
-type rbw >/dev/null 2>&1 && exit
+type bw >/dev/null 2>&1 && exit
 
 source /etc/os-release
 
 if [ "$ID" == "fedora" ]; then
-  sudo dnf install --assumeyes rbw
+  sudo dnf install --assumeyes nodejs npm
+  sudo npm -g install @bitwarden/cli
 elif [ "$ID" == "arch" ]; then
-  sudo pacman -S --noconfirm rbw
+  sudo pacman -S --noconfirm bitwarden-cli
 fi
