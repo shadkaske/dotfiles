69a16185a8ef6c6226dca506b4cabfef2e0ccf01
Shad Kaske <Shad.Kaske@showplacecabinetry.com>
2025-11-01 12:02:27 -0500

    Force login to Bitwarden if we are not already logged in

--- hooks/install-password-manager.sh
+++ hooks/install-password-manager.sh
@@ -1,12 +1,33 @@
 #!/usr/bin/env bash
+
+bw_login() {
+  if [ "$ID" == "fedora" ]; then
+    sudo dnf install --assumeyes jq
+  elif [ "$ID" == "arch" ]; then
+    sudo pacman -S --noconfirm jq
+  fi
+
+  BW_STATUS="$(bw status | jq -r '.status')"
+
+  if [[ "$BW_STATUS" == "unauthenticated" ]]; then
+    echo "Login to Bitwarden: "
+    bw login
+  fi
+}
+
 # exit immediately if password-manager-binary is already in $PATH
-type bw >/dev/null 2>&1 && exit
+if type bw >/dev/null 2>&1; then
+  bw_login
+  exit
+fi
 
 source /etc/os-release
 
 if [ "$ID" == "fedora" ]; then
-  sudo dnf install --assumeyes nodejs npm
+  sudo dnf install --assumeyes nodejs npm jq
   sudo npm -g install @bitwarden/cli
 elif [ "$ID" == "arch" ]; then
-  sudo pacman -S --noconfirm bitwarden-cli
+  sudo pacman -S --noconfirm bitwarden-cli jq
 fi
+
+bw_login
