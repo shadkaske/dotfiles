b82988e1fe479b8d1c8cd9808ad6daee90ec7710
Shad Kaske <shadkaske@gmail.com>
2025-11-15 10:34:08 -0600

    Tmux and Neovim changed resize keybinds to Ctrl Alt

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -7,10 +7,10 @@ vim.keymap.del("n", "<leader>l")
 
 -- Smart Splits
 -- resizing splits
-vim.keymap.set("n", "<A-h>", require("smart-splits").resize_left)
-vim.keymap.set("n", "<A-j>", require("smart-splits").resize_down)
-vim.keymap.set("n", "<A-k>", require("smart-splits").resize_up)
-vim.keymap.set("n", "<A-l>", require("smart-splits").resize_right)
+vim.keymap.set("n", "<C-A-h>", require("smart-splits").resize_left)
+vim.keymap.set("n", "<C-A-j>", require("smart-splits").resize_down)
+vim.keymap.set("n", "<C-A-k>", require("smart-splits").resize_up)
+vim.keymap.set("n", "<C-A-l>", require("smart-splits").resize_right)
 -- moving between splits
 vim.keymap.set("n", "<C-h>", require("smart-splits").move_cursor_left)
 vim.keymap.set("n", "<C-j>", require("smart-splits").move_cursor_down)
